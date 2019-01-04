0a0a72b2a9281764c03e5daa6b52847741f73e2b
Romain <rmtestard@gmail.com>
2019-01-04 07:52:25 +0100

    POST request : User Registration

--- models/User.js
+++ models/User.js
@@ -11,13 +11,12 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
-  paswword: {
+  password: {
     type: String,
     required: true
   },
   avatar: {
-    type: String,
-    required: true
+    type: String
   },
   date: {
     type: Date,
