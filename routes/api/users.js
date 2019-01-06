f2f4f03d57a56d36cdbd12dcf585a537b859ef28
Romain <rmtestard@gmail.com>
2019-01-06 11:44:07 +0100

    server-side validation for registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,9 +24,16 @@ router.get('/test', (req, res) => {
 // @desc    Registration user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
