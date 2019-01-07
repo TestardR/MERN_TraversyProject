a691f0edeac36bbb58d527b43c31e76d3cbefc0d
Romain <rmtestard@gmail.com>
2019-01-07 08:39:48 +0100

    adding validation to our profile

---
+++ validation/profile.js
@@ -0,0 +1,58 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  // Turn into an empty string if empty
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.status = !isEmpty(data.status) ? data.status : '';
+  data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = 'Handle needs to be between 2 and 40 characters';
+  }
+
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = 'Profile handle is required';
+  }
+
+  if (Validator.isEmpty(data.status)) {
+    errors.status = 'Status field is required';
+  }
+
+  if (Validator.isEmpty(data.skills)) {
+    errors.skills = 'Skills field is required';
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      erros.website = 'Not a valid URL';
+    }
+  }
+  if (!isEmpty(data.youtube)) {
+    if (!Validator.isURL(data.youtube)) {
+      erros.youtube = 'Not a valid URL';
+    }
+  }
+  if (!isEmpty(data.twitter)) {
+    if (!Validator.isURL(data.twitter)) {
+      erros.twitter = 'Not a valid URL';
+    }
+  }
+  if (!isEmpty(data.linkedin)) {
+    if (!Validator.isURL(data.linkedin)) {
+      erros.linkedin = 'Not a valid URL';
+    }
+  }
+  if (!isEmpty(data.instagram)) {
+    if (!Validator.isURL(data.instagram)) {
+      erros.instagram = 'Not a valid URL';
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors) // Return if the entered value is valid or not (see isEmpty() for more info).
+  };
+};
