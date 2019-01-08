913476652fcfed03223c8ae7e63ef14de800a19f
Romain <rmtestard@gmail.com>
2019-01-08 17:00:55 +0100

    validators for POST post route

---
+++ validation/post.js
@@ -0,0 +1,22 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  // Turn into an empty string if empty
+  data.text = !isEmpty(data.text) ? data.text : '';
+
+  if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
+    errors.text = 'Post must be between 10 and 300 characters';
+  }
+
+  if (Validator.isEmail(data.text)) {
+    errors.text = 'Text field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors) // Return if the entered value is valid or not (see isEmpty() for more info).
+  };
+};
