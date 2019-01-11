382e21dc960e21189356d34b90a4a662226aa254
Romain <rmtestard@gmail.com>
2019-01-11 09:09:53 +0100

    updating redux store

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const middleware = [thunk]; // recommended to handle async logic (AJAX requests)
+
+const store = createStore(
+  () => [],
+  {},
+  applyMiddleware({ ...applyMiddleware })
+);
 
 export default store;
