1f1058a24398f7a2f4f9482d3c53cf41d34d9bb3
Romain <rmtestard@gmail.com>
2019-01-11 09:16:08 +0100

    combineReducers and authReducer creation

--- client/src/store.js
+++ client/src/store.js
@@ -3,10 +3,6 @@ import thunk from 'redux-thunk';
 
 const middleware = [thunk]; // recommended to handle async logic (AJAX requests)
 
-const store = createStore(
-  () => [],
-  {},
-  applyMiddleware({ ...applyMiddleware })
-);
+const store = createStore(() => [], {}, applyMiddleware({ ...middleware }));
 
 export default store;
