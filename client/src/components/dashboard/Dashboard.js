df184a5a499f461362c718cc73bf7aace412ffb3
Romain <rmtestard@gmail.com>
2019-01-15 18:21:56 +0100

    calling an action redux connected component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,10 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
 
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+
   render() {
     return (
       <div>
