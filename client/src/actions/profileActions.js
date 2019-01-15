2a678393d06e3d918b0703ff6a4f293b237a5e27
Romain <rmtestard@gmail.com>
2019-01-15 18:08:55 +0100

    profile actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,3 +1,30 @@
 import axios from 'axios';
 
 import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from './types';
+
+// Get current profile
+export const getCurrentProfile = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        // if error we want ot return an empty profile
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+// Profile loading
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
