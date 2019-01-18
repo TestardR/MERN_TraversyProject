cb0cdf8efa5276de47fc5dee690ef7c07b6bbfeb
Romain <rmtestard@gmail.com>
2019-01-18 12:33:34 +0100

    Redux flow: addexperience action

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addExperience } from '../../actions/profileActions';
 
 class AddExperience extends Component {
   constructor(props) {
@@ -28,19 +29,17 @@ class AddExperience extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    console.log('submit');
+    const expData = {
+      company: this.state.company,
+      title: this.state.title,
+      location: this.state.location,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    };
 
-    // const expData = {
-    //   company: this.state.company,
-    //   title: this.state.title,
-    //   location: this.state.location,
-    //   from: this.state.from,
-    //   to: this.state.to,
-    //   current: this.state.current,
-    //   description: this.state.description
-    // };
-
-    // this.props.addExperience(expData, this.props.history);
+    this.props.addExperience(expData, this.props.history);
   }
 
   onChange(e) {
@@ -146,7 +145,8 @@ class AddExperience extends Component {
 
 AddExperience.propTypes = {
   profile: PropTypes.object.isRequired,
-  errors: PropTypes.object.isRequired
+  errors: PropTypes.object.isRequired,
+  addExperience: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -154,4 +154,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(
+  mapStateToProps,
+  { addExperience }
+)(withRouter(AddExperience));
