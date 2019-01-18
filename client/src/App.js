c306f3dde1ce992f1de68fc68643874b266588e2
Romain <rmtestard@gmail.com>
2019-01-18 11:17:09 +0100

    AddExperience Component - interface

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import './App.css';
 
@@ -62,6 +63,11 @@ class App extends Component {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </div>
             <Footer />
