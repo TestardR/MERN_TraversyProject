72a6be2392d4e2a6b916a22b92acbde70f3263c9
Romain <rmtestard@gmail.com>
2019-01-20 14:29:44 +0100

    connecting our not found page

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/not-found/NotFound';
 
 import './App.css';
 
@@ -79,6 +80,7 @@ class App extends Component {
                 path="/add-education"
                 component={AddEducation}
               />
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
